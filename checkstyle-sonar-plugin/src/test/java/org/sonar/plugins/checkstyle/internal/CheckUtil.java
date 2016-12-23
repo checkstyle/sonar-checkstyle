f9e7c672c1cf198ce69b1665d6be399b565d5894
rnveach <rveach02@gmail.com>
2016-12-23 18:53:47 -0500

    Issue #28: fix broken rules test

--- checkstyle-sonar-plugin/src/test/java/org/sonar/plugins/checkstyle/internal/CheckUtil.java
+++ checkstyle-sonar-plugin/src/test/java/org/sonar/plugins/checkstyle/internal/CheckUtil.java
@@ -22,40 +22,103 @@ package org.sonar.plugins.checkstyle.internal;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.AbstractFileSetCheck;
+import com.puppycrawl.tools.checkstyle.api.AutomaticBean;
+import com.puppycrawl.tools.checkstyle.api.Filter;
+import com.puppycrawl.tools.checkstyle.guava.collect.ImmutableSet;
+import com.puppycrawl.tools.checkstyle.guava.reflect.ClassPath;
+import com.puppycrawl.tools.checkstyle.guava.reflect.ClassPath.ClassInfo;
 
 public final class CheckUtil {
     private CheckUtil() {
     }
 
     /**
-     * Gets the checkstyle's modules. Checkstyle's modules are nonabstract
-     * classes from com.puppycrawl.tools.checkstyle package which names end with
-     * 'Check', do not contain the word 'Input' (are not input files for UTs),
-     * checkstyle's filters and SuppressWarningsHolder class.
-     *
-     * @return a set of checkstyle's modules names.
+     * Gets all checkstyle's modules.
+     * @return the set of checkstyle's module classes.
      * @throws IOException if the attempt to read class path resources failed.
+     * @see #isCheckstyleModule(Class)
      */
     public static Set<Class<?>> getCheckstyleModules() throws IOException {
         final Set<Class<?>> checkstyleModules = new HashSet<>();
 
-//        final ClassLoader loader = Thread.currentThread().getContextClassLoader();
-//        final ClassPath classpath = ClassPath.from(loader);
-//        final String packageName = "com.puppycrawl.tools.checkstyle.checks";
-//        final ImmutableSet<ClassPath.ClassInfo> checkstyleClasses = classpath
-//                .getTopLevelClassesRecursive(packageName);
-//
-//        for (ClassPath.ClassInfo clazz : checkstyleClasses) {
-//            final Class<?> loadedClass = clazz.load();
-//            if (isCheckstyleModule(loadedClass)) {
-//                checkstyleModules.add(loadedClass);
-//            }
-//        }
+        final ClassLoader loader = Thread.currentThread()
+                .getContextClassLoader();
+        final ClassPath classpath = ClassPath.from(loader);
+        final String packageName = "com.puppycrawl.tools.checkstyle";
+        final ImmutableSet<ClassInfo> checkstyleClasses = classpath
+                .getTopLevelClassesRecursive(packageName);
+
+        for (ClassPath.ClassInfo clazz : checkstyleClasses) {
+            final Class<?> loadedClass = clazz.load();
+            if (isCheckstyleModule(loadedClass)) {
+                checkstyleModules.add(loadedClass);
+            }
+        }
         return checkstyleModules;
+    }    /**
+     * Checks whether a class may be considered as a checkstyle module. Checkstyle's modules are
+     * non-abstract classes, which names do not start with the word 'Input' (are not input files for
+     * UTs), and are either checkstyle's checks, file sets, filters, file filters, or root module.
+     * @param loadedClass class to check.
+     * @return true if the class may be considered as the checkstyle module.
+     */
+    private static boolean isCheckstyleModule(Class<?> loadedClass) {
+        final String className = loadedClass.getSimpleName();
+        return isValidCheckstyleClass(loadedClass, className)
+            && (isCheckstyleCheck(loadedClass)
+                    || isFileSetModule(loadedClass)
+                    || isFilterModule(loadedClass));
+    }
+
+    /**
+     * Checks whether a class extends 'AutomaticBean', is non-abstract, and doesn't start with the
+     * word 'Input' (are not input files for UTs).
+     * @param loadedClass class to check.
+     * @param className class name to check.
+     * @return true if a class may be considered a valid production class.
+     */
+    public static boolean isValidCheckstyleClass(Class<?> loadedClass, String className) {
+        return AutomaticBean.class.isAssignableFrom(loadedClass)
+                && !Modifier.isAbstract(loadedClass.getModifiers())
+                && !className.contains("Input");
+    }
+
+    /**
+     * Checks whether a class may be considered as the checkstyle check.
+     * Checkstyle's checks are classes which implement 'AbstractCheck' interface.
+     * @param loadedClass class to check.
+     * @return true if a class may be considered as the checkstyle check.
+     */
+    public static boolean isCheckstyleCheck(Class<?> loadedClass) {
+        return AbstractCheck.class.isAssignableFrom(loadedClass);
+    }
+
+    /**
+     * Checks whether a class may be considered as the checkstyle file set.
+     * Checkstyle's file sets are classes which implement 'AbstractFileSetCheck' interface.
+     * @param loadedClass class to check.
+     * @return true if a class may be considered as the checkstyle file set.
+     */
+    public static boolean isFileSetModule(Class<?> loadedClass) {
+        return AbstractFileSetCheck.class.isAssignableFrom(loadedClass);
+    }
+
+    /**
+     * Checks whether a class may be considered as the checkstyle filter.
+     * Checkstyle's filters are classes which implement 'Filter' interface.
+     * @param loadedClass class to check.
+     * @return true if a class may be considered as the checkstyle filter.
+     */
+    public static boolean isFilterModule(Class<?> loadedClass) {
+        return Filter.class.isAssignableFrom(loadedClass);
     }
 
     /**
