a84174cb12d533e0c4ff346c7e31948cf764d291
Andrei Paikin <andreypaykin@gmail.com>
2019-06-14 12:42:00 +0300

    minor: add javadoc to package-info

--- src/main/java/org/sonar/plugins/checkstyle/package-info.java
+++ src/main/java/org/sonar/plugins/checkstyle/package-info.java
@@ -16,6 +16,9 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 ////////////////////////////////////////////////////////////////////////////////
+/**
+ * Provides the classes for plugin.
+ */
 @ParametersAreNonnullByDefault
 
 package org.sonar.plugins.checkstyle;
