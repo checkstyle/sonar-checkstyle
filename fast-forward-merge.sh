543872a038a53252097bb8f9b3bde533e3c51911
Roman Ivanov <ivanov-jr@mail.ru>
2019-06-14 23:00:56 -0700

    Issue #225: resolve IDEA violations from BashSupport

--- fast-forward-merge.sh
+++ fast-forward-merge.sh
@@ -6,38 +6,38 @@
 
 set -e
 
-if [ $# -eq 0 ]
+if [ "$#" -eq 0 ]
   then
     echo "$(basename "$0") FORK_USER_NAME USER_BRANCH
 example:
 
     ./$(basename "$0") konstantinos issue73
 "
-    exit 0;
+    exit "0";
 fi
 
 GIT_REPO=sonar-checkstyle
-FORK_USER_NAME=$1
-USER_BRANCH=$2
+FORK_USER_NAME="$1"
+USER_BRANCH="$2"
 REPO=${FORK_USER_NAME}-fork
 LOCAL_USER_BRANCH=${FORK_USER_NAME}-${USER_BRANCH}
 
 echo "adding remote ..."
-git remote add ${REPO} https://github.com/${FORK_USER_NAME}/${GIT_REPO}.git
-git fetch ${REPO}
+git remote add "${REPO}" https://github.com/${FORK_USER_NAME}/${GIT_REPO}.git
+git fetch "${REPO}"
 
 echo "creating local branch ..."
-git checkout -b ${LOCAL_USER_BRANCH} ${REPO}/${USER_BRANCH}
+git checkout -b "${LOCAL_USER_BRANCH}" ${REPO}/${USER_BRANCH}
 
 echo "rebasing over master ..."
 git rebase master
 
 echo "merge to master ..."
 git checkout master
-git merge ${LOCAL_USER_BRANCH} --ff-only
+git merge "${LOCAL_USER_BRANCH}" --ff-only
 
 echo "removing local branch ..."
-git branch -D ${LOCAL_USER_BRANCH}
+git branch -D "${LOCAL_USER_BRANCH}"
 
 echo "removing remote ..."
-git remote rm ${REPO}
+git remote rm "${REPO}"
