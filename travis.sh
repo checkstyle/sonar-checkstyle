892ea8b628477157eb9c72880e99f8c5d62d7337
Roman Ivanov <ivanov-jr@mail.ru>
2019-06-14 23:00:56 -0700

    Issue #225: resolve IDEA violations from BashSupport

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ ci)
   ;;
 
 sonar)
-  if [[ $TRAVIS_PULL_REQUEST =~ ^([0-9]*)$ ]]; then exit 0; fi
+  if [[ "$TRAVIS_PULL_REQUEST" =~ ^([0-9]*)$ ]]; then exit "0"; fi
   mvn clean \
     org.jacoco:jacoco-maven-plugin:prepare-agent \
     package \
@@ -17,21 +17,21 @@ sonar)
     -Dsonar.projectKey=checkstyle_sonar-checkstyle \
     -Dsonar.organization=checkstyle \
     -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.login=$SONAR_TOKEN
+    -Dsonar.login=${SONAR_TOKEN}
   ;;
 
 nondex)
   mvn --fail-never clean nondex:nondex -Dcheckstyle.skip=true
-  cat `grep -RlE 'td class=.x' .nondex/ | cat` < /dev/null > output.txt
-  RESULT=$(cat output.txt | wc -c)
+  cat "`grep -RlE 'td class=.x' .nondex/ | cat`" < /dev/null > output.txt
+  RESULT="$(cat output.txt | wc -c)"
   cat output.txt
-  echo 'Size of output:'$RESULT
-  if [[ $RESULT != 0 ]]; then false; fi
+  echo 'Size of output:'${RESULT}
+  if [[ "$RESULT" != 0 ]]; then false; fi
   ;;
 
 *)
   echo "Unexpected TEST mode: $TEST"
-  exit 1
+  exit "1"
   ;;
 
 esac
