74e6a5cc7159b01beef94ba3c3837bc6eb82294d
Daniel Muehlbachler <daniel@muehlbachler.org>
2019-09-10 15:36:50 +0200

    Issue #231: update to CS 8.22

--- src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
+++ src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
@@ -61,7 +61,7 @@ public class CheckstyleRulesDefinitionTest {
         assertThat(repository.language()).isEqualTo("java");
 
         final List<RulesDefinition.Rule> rules = repository.rules();
-        assertThat(rules).hasSize(159);
+        assertThat(rules).hasSize(163);
 
         for (RulesDefinition.Rule rule : rules) {
             assertThat(rule.key()).isNotNull();
