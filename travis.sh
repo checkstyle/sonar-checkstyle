e31ba7c3ef35bec493c30be82306f27701f7cbd5
Roman Ivanov <ivanov-jr@mail.ru>
2020-01-17 15:25:54 -0800

    Issue #262: use wercker for Sonarqube.io validation in PRs

--- travis.sh
+++ travis.sh
@@ -8,18 +8,6 @@ install)
   mvn -e clean install
   ;;
 
-sonar)
-  if [[ "$TRAVIS_PULL_REQUEST" =~ ^([0-9]*)$ ]]; then exit "0"; fi
-  mvn clean \
-    org.jacoco:jacoco-maven-plugin:prepare-agent \
-    package \
-    sonar:sonar \
-    -Dsonar.projectKey=checkstyle_sonar-checkstyle \
-    -Dsonar.organization=checkstyle \
-    -Dsonar.host.url=https://sonarcloud.io \
-    -Dsonar.login=${SONAR_TOKEN}
-  ;;
-
 nondex)
   mvn --fail-never clean nondex:nondex -Dcheckstyle.skip=true
   cat `grep -RlE 'td class=.x' .nondex/ | cat` < /dev/null > output.txt
