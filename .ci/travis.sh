04c24344128fcce61f9de37131338ac706fdd3ee
Roman Ivanov <ivanov-jr@mail.ru>
2020-01-17 16:39:23 -0800

    infra: put sonar-application-7.9.2.zip to maven cache

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -5,6 +5,12 @@ set -euo pipefail
 case "$1" in
 
 install)
+  SONAR_APP_VERSION="7.9.2"
+  if [[ ! -f ~/.m2/sonar-application-$SONAR_APP_VERSION.zip ]]; then
+    URL="https://repox.jfrog.io/repox/sonarsource/org/sonarsource/sonarqube/"
+    URL=$URL"sonar-application/$SONAR_APP_VERSION/sonar-application-$SONAR_APP_VERSION.zip"
+    wget $URL -O ~/.m2/sonar-application-$SONAR_APP_VERSION.zip
+  fi
   mvn -e clean install
   ;;
 
