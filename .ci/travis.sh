2be5b075816c31d11b25d3315d0522084f3b1253
Roman Ivanov <ivanov-jr@mail.ru>
2020-01-19 16:10:32 -0800

    infra: add ITs placeholder

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -5,13 +5,17 @@ set -euo pipefail
 case "$1" in
 
 install)
+  mvn -e clean install
+  ;;
+
+integration-tests)
   SONAR_APP_VERSION="7.9.2"
   if [[ ! -f ~/.m2/sonar-application-$SONAR_APP_VERSION.zip ]]; then
     URL="https://repox.jfrog.io/repox/sonarsource/org/sonarsource/sonarqube/"
     URL=$URL"sonar-application/$SONAR_APP_VERSION/sonar-application-$SONAR_APP_VERSION.zip"
     wget $URL -O ~/.m2/sonar-application-$SONAR_APP_VERSION.zip
   fi
-  mvn -e clean install
+  mvn -e integration-test -DskipITs=false
   ;;
 
 nondex)
