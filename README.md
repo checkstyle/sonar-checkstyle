e0d4eab72d799b951a54a75799b4146852b082c1
Daniel Muehlbachler <daniel@muehlbachler.org>
2020-05-14 09:31:23 +0200

    Issue #292: update to CS 8.32

--- README.md
+++ README.md
@@ -21,6 +21,7 @@ Compatibility matrix from checkstyle team:
 
 Checkstyle Plugin|Sonar min|Sonar max|Checkstyle|Jdk
 -----------------|---------|---------|----------|---
+4.32|7.9  |7.9+|8.32|1.8
 4.31|7.9  |7.9+|8.31|1.8
 4.30|7.9  |7.9+|8.30|1.8
 4.29|7.9  |7.9+|8.29|1.8
--- src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
+++ src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
@@ -61,7 +61,7 @@ public class CheckstyleRulesDefinitionTest {
         assertThat(repository.language()).isEqualTo("java");
 
         final List<RulesDefinition.Rule> rules = repository.rules();
-        assertThat(rules).hasSize(172);
+        assertThat(rules).hasSize(173);
 
         for (RulesDefinition.Rule rule : rules) {
             assertThat(rule.key()).isNotNull();
