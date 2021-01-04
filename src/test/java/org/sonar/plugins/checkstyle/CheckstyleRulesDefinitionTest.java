85fdf27eb83d97ea4e39208fb0ce8a011c82aef4
Daniel Muehlbachler-Pietrzykowski <daniel@muehlbachler.io>
2021-01-04 10:02:38 +0100

    Issue #351: update to CS 8.39

--- src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
+++ src/test/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinitionTest.java
@@ -80,7 +80,7 @@ public class CheckstyleRulesDefinitionTest {
         // such number should not change during checkstyle version upgrade
         assertThat(duplicatedRuleWithTemplate).hasSize(174);
         // all new Rules should fall in this group
-        assertThat(rulesWithDuplicateTemplate).hasSize(7);
+        assertThat(rulesWithDuplicateTemplate).hasSize(8);
 
         for (RulesDefinition.Rule rule : rules) {
             assertThat(rule.key()).isNotNull();
