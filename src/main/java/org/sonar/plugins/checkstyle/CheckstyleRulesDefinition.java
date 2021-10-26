263e7c877688ed39d33f11769e9f2ce04c855494
rnveach <rveach02@gmail.com>
2021-10-26 09:38:13 -0400

    Issue #378: remove outdated checkstyle integration code

--- src/main/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinition.java
+++ src/main/java/org/sonar/plugins/checkstyle/CheckstyleRulesDefinition.java
@@ -19,20 +19,10 @@
 
 package org.sonar.plugins.checkstyle;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
-import java.util.Properties;
-
 import org.sonar.api.ExtensionPoint;
 import org.sonar.api.batch.ScannerSide;
 import org.sonar.api.server.rule.RulesDefinition;
-import org.sonar.api.server.rule.RulesDefinitionXmlLoader;
 import org.sonar.plugins.checkstyle.metadata.CheckstyleMetadata;
-import org.sonar.squidbridge.rules.ExternalDescriptionLoader;
-import org.sonar.squidbridge.rules.SqaleXmlLoader;
-
-import com.google.common.annotations.VisibleForTesting;
 
 @ExtensionPoint
 @ScannerSide
@@ -43,65 +33,10 @@ public final class CheckstyleRulesDefinition implements RulesDefinition {
         final NewRepository repository = context.createRepository(
                 CheckstyleConstants.REPOSITORY_KEY, "java").setName(
                 CheckstyleConstants.REPOSITORY_NAME);
-        try {
-            extractRulesData(repository, "/org/sonar/plugins/checkstyle/rules.xml",
-                    "/org/sonar/l10n/checkstyle/rules/checkstyle");
-        }
-        catch (IOException ex) {
-            throw new IllegalStateException("Exception during extractRulesData", ex);
-        }
 
         final CheckstyleMetadata checkstyleMetadata = new CheckstyleMetadata(repository);
         checkstyleMetadata.createRulesWithMetadata();
 
         repository.done();
-    }
-
-    @VisibleForTesting
-    static void extractRulesData(NewRepository repository, String xmlRulesFilePath,
-                                 String htmlDescriptionFolder) throws IOException {
-        final RulesDefinitionXmlLoader ruleLoader = new RulesDefinitionXmlLoader();
-        try (InputStream resource = CheckstyleRulesDefinition.class
-                .getResourceAsStream(xmlRulesFilePath)) {
-            ruleLoader.load(repository, resource, "UTF-8");
-        }
-        ExternalDescriptionLoader.loadHtmlDescriptions(repository, // NOSONAR
-                htmlDescriptionFolder);
-        try (InputStream resource = CheckstyleRulesDefinition.class
-                .getResourceAsStream("/org/sonar/l10n/checkstyle.properties")) {
-            loadNames(repository, resource);
-        }
-        SqaleXmlLoader.load(repository, "/com/sonar/sqale/checkstyle-model.xml"); // NOSONAR
-    }
-
-    private static void loadNames(NewRepository repository, InputStream stream) {
-        // code taken from:
-        // https://github.com/SonarSource/sslr-squid-bridge/blob/2.5.2/
-        // src/main/java/org/sonar/squidbridge/rules/PropertyFileLoader.java#L46
-        final Properties properties = new Properties();
-        try {
-            properties.load(stream);
-        }
-        catch (IOException ex) {
-            throw new IllegalArgumentException("Could not read names from properties", ex);
-        }
-
-        if (Objects.nonNull(repository.rules())) {
-            repository.rules().forEach(rule -> {
-                final String baseKey = "rule." + repository.key() + "." + rule.key();
-                final String nameKey = baseKey + ".name";
-                final String ruleName = properties.getProperty(nameKey);
-                if (Objects.nonNull(ruleName)) {
-                    rule.setName(ruleName);
-                }
-                rule.params().forEach(param -> {
-                    final String paramDescriptionKey = baseKey + ".param." + param.key();
-                    final String paramDescription = properties.getProperty(paramDescriptionKey);
-                    if (Objects.nonNull(paramDescription)) {
-                        param.setDescription(paramDescription);
-                    }
-                });
-            });
-        }
     }
 }
